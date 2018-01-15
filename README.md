528c489b8c5df8d54bb3600d519dfc4f084774c4
suncle <snowlight437@gmail.com>
2018-01-15 15:39:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+腾讯云提供的cmq python sdk没有分发包，因此稍作修改，增加打包部分
+
+使用方法：下载dist目录下的压缩包之后解压然后执行下面的命令即可完成安装
+
+```bash
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='qc_cmq_python_sdk',
+    version='1.0.4',
+    description='tencent cmq python sdk packaging',
+    url='https://github.com/Flowsnow/qc_cmq_python_sdk',
+    author='Flowsnow',
+    author_email='snowlight437@gmail.com',
+    keywords='cmq tencent python',
+    packages=['cmq'],
+)
